6b3b82efd317b1af4d1529d75ae07a888d87c052
朱 天明 <shutenmei@shutekiMacBook-Pro.local>
2016-12-07 21:28:00 -0600

    First time push

---
+++ app/src/main/java/shutenmei/example/com/fragmenttest/Fragment2.java
@@ -0,0 +1,22 @@
+package shutenmei.example.com.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by LRL on 4/5/2016.
+ */
+public class Fragment2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_fragment2,container,false);
+        return v;
+    }
+}
---
+++ app/src/main/java/shutenmei/example/com/fragmenttest/MainActivity.java
@@ -0,0 +1,28 @@
+package shutenmei.example.com.fragmenttest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn=(Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
